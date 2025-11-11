0f7085ab4496dfae56e263efefe2b502641c58e1
Student <nguyenanthonyd4@gmail.com>
2025-11-11 14:28:19 -0800

    Reapply "Pseudocode for order and Macrobowl class"
    This reverts commit 8ac06212bb66998a2e6f187562274528cd4fc967.

---
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -0,0 +1,50 @@
+package com.pluralsight;
+
+public class UserInterface {
+/*  private priceTable PriceTable
+    private menu  menuData
+    private Scanner
+
+UserInterface(priceTable, menu)
+   this.priceTable = priceTable
+    this.menu = menu
+    new Scanner for user input
+
+
+
+
+
+
+
+
+Method startNewOrDER()
+Create new order
+loop
+display " Order Screen"
+display all orders (newest first)
+display:
+1) Add Bowl
+2) Add Drink
+3) Add Main Side
+4) Checkout
+0) Cancel Order - delete order and go back to home page
+prompt user choice
+
+if choice == 1 then
+addBowl(order)
+else if choice == 2 then
+addDrink(order)
+else if choice == 3 then
+addSide(order)
+else if choice == 4 then
+checkout(order)
+break // return to home screen
+else if choice == 0 then
+print "order cancelled"
+break
+else print "invalid choice, try again."
+end if
+end loop end method
+
+ */
+}
