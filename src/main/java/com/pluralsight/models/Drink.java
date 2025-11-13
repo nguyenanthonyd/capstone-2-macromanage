60962b72e15c434dfc819af76c7c52f5472bfed5
Student <nguyenanthonyd4@gmail.com>
2025-11-12 21:17:26 -0800

    updated methods for drink and sides class

--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -1,6 +1,27 @@
 package com.pluralsight.models;
 
+import com.pluralsight.models.enums.DrinkType;
+import com.pluralsight.models.enums.Size;
+
 public class Drink {
+    private final DrinkType type;
+    private final Size size;
+
+    public Drink(DrinkType type, Size size) {
+        this.type = type;
+        this.size = size;
+    }
+
+    public double getPrice(PriceTable priceTable) {
+        return priceTable.drink(size);
+    }
+
+    @Override
+    public String toString() {
+        return type + " (" + size + ")";
+    }
+}
+
 
   /*  properties:
         type // Green fiend , acai potion, Protein Powah ,
--- src/main/java/com/pluralsight/models/Side.java
+++ src/main/java/com/pluralsight/models/Side.java
@@ -1,6 +1,22 @@
 package com.pluralsight.models;
 
 public class Side {
+    private final String name;
+
+    public Side(String name) {
+        this.name = name;
+    }
+
+    public double getPrice(PriceTable priceTable) {
+        return priceTable.side();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
 
     /* properties:
         name //
