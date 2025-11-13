55dc2346b18174f6f4fdf02abb4a1e61df3b8444
Student <nguyenanthonyd4@gmail.com>
2025-11-12 18:41:00 -0800

    updated MacroBowl class with helper methods and getter

--- src/main/java/com/pluralsight/models/MacroBowl.java
+++ src/main/java/com/pluralsight/models/MacroBowl.java
@@ -3,13 +3,127 @@ package com.pluralsight.models;
 import com.pluralsight.models.enums.BaseType;
 import com.pluralsight.models.enums.Size;
 import com.pluralsight.models.enums.SpecialOption;
+import com.pluralsight.models.enums.ToppingCategory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MacroBowl {
+
     private Size size;
     private BaseType baseType;
-    private List<ToppingSelection> topping;
-    private SpecialOption specialOption;
+    private List<ToppingSelection> toppings;
+    private SpecialOption specialOption = SpecialOption.NONE;
+
+    public MacroBowl(Size size, BaseType baseType) {
+        this.size = size;
+        this.baseType = baseType;
+        this.toppings = new ArrayList<>();
+    }
+
+    // method to add any topping
+    // uses preventsExtra() to disable "extra" for regular toppings/condiments
+    public void addTopping(Topping topping, boolean extra) {
+        if (topping.preventsExtra()) {
+            extra = false; // regular toppings + condiments cannot be extra
+        }
+        toppings.add(new ToppingSelection(topping, extra));
+    }
+
+    // helper methods
+
+    public void addProtein(Topping topping, boolean extra) {
+        if (topping.getCategory() == ToppingCategory.PROTEIN) {
+            addTopping(topping, extra);
+        }
+    }
+
+    public void addPremiumTopping(Topping topping, boolean extra) {
+        if (topping.getCategory() == ToppingCategory.PREMIUM) {
+            addTopping(topping, extra);
+        }
+    }
+
+    public void addRegularTopping(Topping topping) {
+        if (topping.getCategory() == ToppingCategory.REGULAR_TOPPING) {
+            addTopping(topping, false);
+        }
+    }
+
+    public void addCondiment(Topping topping) {
+        if (topping.getCategory() == ToppingCategory.CONDIMENTS) {
+            addTopping(topping, false);
+        }
+    }
+
+    public void setSpecialOption(SpecialOption option) {
+        this.specialOption = option;
+    }
+
+    // Price calculation
+    public double getPrice(PriceTable priceTable) {
+        double total = priceTable.baseBowl(size);
+
+        for (ToppingSelection toppingSelection : toppings) {
+            Topping topping = toppingSelection.getTopping();
+            total += priceTable.toppingCharge(
+                    topping.getCategory(),
+                    size,
+                    toppingSelection.isExtra(),
+                    topping.isPremium()
+            );
+        }
+
+        total += priceTable.specialSurcharge(specialOption, size);
+
+        return total;
+    }
+
+    // Receipt order screen description
+
+    public String description() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append(size).append(" ").append(baseType).append(" bowl");
+
+        if (!toppings.isEmpty()) {
+            stringBuilder.append(" with ");
+            for (int i = 0; i < toppings.size(); i++) {
+                stringBuilder.append(toppings.get(i).label());
+                if (i < toppings.size() - 1) {
+                    stringBuilder.append(", ");
+                }
+            }
+        } else {
+            StringBuilder.append(" (no toppings)");
+        }
+
+        if (specialOption != SpecialOption.NONE) {
+            StringBuilder.append(" | ").append(specialOption);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    // Getters
+    public Size getSize() {
+        return size;
+    }
+
+    public BaseType getBaseType() {
+        return baseType;
+    }
+
+    public SpecialOption getSpecialOption() {
+        return specialOption;
+    }
+
+    public List<ToppingSelection> getToppings() {
+        return new ArrayList<>(toppings);
+    }
 }
+
+
     /* properties:
     size ( small,medium,large)
     baseType base
@@ -48,4 +162,3 @@ public class MacroBowl {
 }
 
  */
-}
